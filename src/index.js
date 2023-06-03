7c72f81983c9e410292c51f7ca8dfae370ff4d48
sarabheshwaran <81975881+sarabheshwaran@users.noreply.github.com>
2023-06-03 18:08:46 +0530

    fivth commit

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ root.render(
   <AppProvider>
     <BrowserRouter>
       <Routes>
+        
         <Route path = "/" element = {<Home />}>
           <Route path = "about" element = {<About />} />
           <Route path = "book" element = {<BookList />} />
