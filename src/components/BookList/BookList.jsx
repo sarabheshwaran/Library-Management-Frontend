4159a9079e400752218daaff5e9460e48fce0d92
sarabheshwaran <81975881+sarabheshwaran@users.noreply.github.com>
2023-06-03 17:55:39 +0530

    frth

--- src/components/BookList/BookList.jsx
+++ src/components/BookList/BookList.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useGlobalContext } from '../../context.';
 import Book from "../BookList/Book";
-
+import Loading from "../Loader/Loader";
 import coverImg from "../../images/cover_not_found.jpg";
 import "./BookList.css";
 
@@ -19,7 +19,7 @@ const BookList = () => {
   });
 
  
-
+  if(loading) return <Loading />;
   return (
     <section className='booklist'>
       <div className='container'>
